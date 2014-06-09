3f3bd546dd616d81e08690aa1ac9ef8b2a595e94
Yuko Kiriya <kiriya@ap-com.co.jp>
2014-06-09 17:06:16 +0900

    redis設定修正。セッション切れエラー追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-end
+
+  rescue_from ActionController::InvalidAuthenticityToken do |exception|
+    respond_to do |format|
+      format.html do
+        render :template => "login/login.html", :layout => "application", :status => :precondition_failed
+      end
+    end
+  end
+  end
--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -5,34 +5,29 @@ class LoginController < ApplicationController
 
   def login
     if params[:login_id].present? || params[:password].present?
-      if params[:login_id] == 'kiriya'
+      if params[:login_id] == 'kuma'
         password      = params[:password]
         login_id      = params[:login_id]
         password_hash = Digest::SHA256.hexdigest("#{SALT}#{login_id}#{password}")
 
-
-
-        # _study_rails_session
-        #result = redis.set(login_id, params[:authenticity_token])
-
-        # sessionを作ると、勝手にkeyを払いだされる
-        # passwordはやめてtimestanpとかにする
       end
     end
     # エラー
-    #redirect_to :action => 'show', :error => 'error'
+    #redirect_to :action => 'login', :error => 'error'
   end
 
   def index
-    p "///////"
-    p session.keys
-    p "#######"
-    p cookies['_session_id']
-    pp session['session_id']
-
-    #redis = Redis.new
-    #redis.set(cookies['session_id'], '（・(エ)・）')
-    p "///////"
+    if session['login_id'].blank?
+      session['login_id'] = params[:login_id]
+    end
     #reset_session
+  end
+
+  def encode(val)
+    Marshal.dump val
+  end
+
+  def decode(val)
+    Marshal.load val
   end
 end
